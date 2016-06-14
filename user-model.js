15246809d1c0c689cd16bec143146d7bafcea594
won <wjin0352@gmail.com>
2016-06-14 18:11:29 -0400

    git ignore file, user model, npm, mongoose, index.js

---
+++ user-model.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+var User = mongoose.model('User', UserSchema);
+
+module.export = User;
+
